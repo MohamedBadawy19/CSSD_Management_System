089f6e5ba9bb0f046da24ce714c9fd2c548d4426
OmargamalH <omargamalhamedb8@gmail.com>
2026-04-19 01:08:52 +0200

    add submit instrument requests

--- CSSD_Management_System/CSSD_Management_System/views.py
+++ CSSD_Management_System/CSSD_Management_System/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import  login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import EmailLoginForm
-from .decorators import cssd_staff_required
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
+from .models import InstrumentSet , RequestItem , InstrumentRequest , InventoryItem
+@csrf_exempt
 def login_view(request):
     # Determine which template to show based on a URL parameter
     role_type = request.GET.get('role', 'staff')
@@ -12,9 +13,11 @@ def login_view(request):
 
     if request.method == 'POST':
         form = EmailLoginForm(request, data=request.POST)
+        print(form.is_valid())
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            
             return redirect('dashboard_router')
     else:
         form = EmailLoginForm()
@@ -24,11 +27,14 @@ def login_view(request):
 
 def home(request):
     # Instead of a redirect, render your selection page
+    if request.user.is_authenticated:
+        return redirect('dashboard_router')
     return render(request, 'index.html')
 
 def logout_view(request):
     logout(request)
     return redirect('login')
+
 
 @login_required
 def dashboard_router(request):
@@ -37,26 +43,69 @@ def dashboard_router(request):
     Detects the user’s role and routes them to their corresponding dashboard.
     """
     role = request.user.role
+    
     if role in ['CSSD Technician', 'System Administrator', 'Hospital Administrator']:
-        return redirect('cssd_dashboard')
+        return render(request , 'cssd-dashboard.html')
     elif role == 'Department Nurse':
         return redirect('nurse_dashboard')
     else:
         return HttpResponse(f"Role '{role}' not found. Please contact admin.", status=403)
 
-@login_required
-@cssd_staff_required
-def cssd_dashboard(request):
-    """
-    Dashboard for CSSD Staff (Technicians, Admins, etc.)
-    Blocked for Nurses.
-    """
-    return HttpResponse(f"<h1>CSSD Staff Dashboard</h1><p>Welcome, {request.user.email} (Role: {request.user.role})</p><a href='/logout/'>Logout</a>")
+
 
 @login_required
 def nurse_dashboard(request):
-    """
-    Dashboard for Department Nurses.
-    """
-    return HttpResponse(f"<h1>Department Nurse Dashboard</h1><p>Welcome, {request.user.email} (Role: {request.user.role})</p><a href='/logout/'>Logout</a>")
+    
+    return render(request , 'nurse-dashboard.html' )
 
+def get_instruments():
+    instruments = []
+    for instrument in InventoryItem.objects.all():
+        instrument_dict = {
+            'name' : instrument.name,
+            'category' : instrument.category,
+            'current_stock' : instrument.current_stock,
+            'min_threshold' : instrument.min_threshold,
+
+        }
+
+        instruments.append(instrument_dict)
+    return instruments 
+
+@login_required
+def nurse_create_request(request):
+    
+        
+    return render(request , 'nurse-create-request.html' , {'instruments' : get_instruments()})
+
+
+@csrf_exempt
+def save_instrument_request(request):
+    if request.method == 'POST':
+
+        instruments = request.POST.getlist('instruments')
+        priority = request.POST.get('priority')
+        notes = request.POST.get('notes')
+
+        new_request = InstrumentRequest.objects.create(
+            requester = request.user,
+            priority = priority,
+            department = request.user.department,
+            notes = notes
+        )
+
+        for instrument in instruments:
+            quantity = int(request.POST.get("quantity_" + instrument))
+            database_instrument = InventoryItem.objects.get(name = instrument)
+            
+            if quantity > database_instrument.current_stock:
+                return render(request , 'nurse-create-request.html' , {'instruments' : get_instruments() , 'warning': f'instrument {instrument} has current_stock : {database_instrument.current_stock}' })
+
+            RequestItem.objects.create(
+                request = new_request,
+                inventory_item = database_instrument,
+                quantity = quantity
+            )
+            database_instrument.current_stock -= quantity
+            database_instrument.save()
+    return redirect('nurse_create_request')
