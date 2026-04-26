7d6a33f9b6cc83fa1f4136d7410901d7d5396aa2
Mohamed Ehab <medo.ehab2005@gmail.com>
2026-04-26 14:22:34 +0300

    frontend fixed

--- CSSD_Management_System/CSSD_Management_System/forms.py
+++ CSSD_Management_System/CSSD_Management_System/forms.py
@@ -3,13 +3,18 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
 
 class EmailLoginForm(AuthenticationForm):
+    error_messages = {
+        'invalid_login': _('Incorrect email or password. Please try again.'),
+        'inactive': _('This account is inactive. Please contact the administrator.'),
+    }
+
     username = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'form-control',
+        'class': 'form-input',
         'placeholder': 'Email',
         'id': 'inputEmail'
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control',
+        'class': 'form-input',
         'placeholder': 'Password',
-        'id': 'inputPassword'
+        'id': 'password'
     }))
